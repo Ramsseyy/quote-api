d978fb44b83b17a0bc927ed330a5ed296e832c2a
Ramsseyy <94022198+Ramsseyy@users.noreply.github.com>
2022-11-03 14:11:03 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import "./App.css"
+
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState(' ');
+
+  useEffect(() => {
+    getQuote()
+  }, []);
+
+  const getQuote = () => {
+    let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+    let dataQuotes = data.quotes;
+    let randomNum = Math.floor(Math.random() * dataQuotes.length);
+    let randomQuote = dataQuotes[randomNum];
+
+    setQuote(randomQuote.quote);
+    setAuthor(randomQuote.author);
+    })
+  }
+
+  const handleClick = () => {
+    getQuote();
+  }
+
+  return (
+    <div className="Container">
+      <div className="text"><p>{quote} </p></div>
+      <div className="author"><p>-{author} </p></div>
+      <button onClick={handleClick} className="new">Get New Quote</button>
+    </div>
+  )
+} 
+
+
+export default App;
